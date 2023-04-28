fcbfd8ef214ae331d4f448007f228e0b896d4fd0
Ari Gunawan <ts.arigunawan@gmail.com>
2023-04-28 22:34:42 +0700

    editing navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
-// import HeroImage from "../assets/heroImage.png";
+import HeroImage from "../assets/heroImage.png";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-san-juan-800 via-san-juan-900 to-san-juan-950">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-          {/* <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer</h2>
-          <p className="text-gray-500 py-4 max-w-md">I have 8 years of experience buildong and designing software. Currently, i love to work on web application using technologies like React, Tailwind, Next JS and GraphQL.</p> */}
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">Front-End React Developer</h2>
+          <p className="text-gray-500 py-4 max-w-md">Hy, I'm Wahyu Gunawan. A passionate front-end React developer. </p>
           <div>
-            <p className="py-4 max-w-md text-white text-5xl">This Site Under Construction</p>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-san-juan-600 to-san-juan-800 cursor-pointer drop-shadow-md ">
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
-        <div>{/* <img src={HeroImage} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full" /> */}</div>
+        <div>
+          <img src={HeroImage} alt="my profile" className=" rounded-md mx-auto w-2/3 transition-all duration-300 md:w-full filter grayscale hover:grayscale-0" />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,15 @@ module.exports = {
     fontFamily: {
       signature: ["Great Vibes"],
     },
+    colors: {
+      "san-juan": {
+        600: "#2d748d",
+        800: "#2C5364",
+        900: "#203A43",
+        950: "#0F2027",
+      },
+      white: "#FFF",
+    },
   },
   plugins: [],
 };
